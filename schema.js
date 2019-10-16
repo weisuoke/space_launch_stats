93e402d9e2ae134a0643106a39e7a24b61fff3f4
梅林的法杖 <530264127@qq.com>
2019-10-16 22:52:35 +0800

    feat(schema.js): 新增RootQuery

--- schema.js
+++ schema.js
@@ -2,8 +2,12 @@ const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
-  GraphQLBoolean
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
 } = require("graphql");
+
+const axios = require("axios");
 
 const LaunchType = new GraphQLObjectType({
   name: "Launch",
@@ -24,4 +28,22 @@ const RocketType = new GraphQLObjectType({
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    Launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/launches")
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
