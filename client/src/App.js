b42984fa03d948922f37683e2e31eab7803014a3
梅林的法杖 <530264127@qq.com>
2019-10-17 09:24:16 +0800

    feat: 新增前端项目

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src={logo}
+        alt="Spacex"
+        style={{ width: 300, display: "block", margin: "auto" }}
+      />
+    </div>
+  );
+}
+
+export default App;
